09ac9dd2e92d83f7df0a3c3139eb53fb41478edf
Anthony K <antonyk@users.noreply.github.com>
2020-08-11 13:44:00 -0400

    changed primary and secondary colors

--- src/styles/theme-overrides.js
+++ src/styles/theme-overrides.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '@primary-color': '#ec3944',
+  '@primary-color': '#3BB54A',
   '@link-color': ' #ec3944',
   '@success-color': '#52c41a',
   '@warning-color': '#faad14',
@@ -7,7 +7,7 @@ module.exports = {
   '@font-size-base': '14px',
   '@heading-color': 'rgba(0, 0, 0, 0.85)',
   '@text-color': 'rgba(0, 0, 0, 0.65)',
-  '@text-color-secondary': 'rgba(0, 0, 0, 0.45)',
+  '@text-color-secondary': '#2E8AF7', // 'rgba(0, 0, 0, 0.45)',
   '@disabled-color': 'rgba(0, 0, 0, 0.25)',
   '@border-radius-base': '2px',
   '@border-color-base': '#d9d9d9',
