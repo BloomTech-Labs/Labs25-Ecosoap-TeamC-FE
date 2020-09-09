65a14ab380db778f35fbc7eed51c11226191d5d3
Mario Fernandez <diddleslip@gmail.com>
2020-09-09 16:10:47 -0400

    Added query for markers and got markers displaying in map page

--- src/components/pages/Map/Map.js
+++ src/components/pages/Map/Map.js
@@ -1,11 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import ReactMapGL, { Marker, Popup } from 'react-map-gl';
+import { useQuery, useMutation, gql } from '@apollo/client';
 import './Map.css';
+// import * as parkData from "./Skateboard_Parks.json";
 
 const REACT_APP_MAPBOX_TOKEN =
   'pk.eyJ1IjoibGFtYmRhbGFiczI1ZWNvc29hcCIsImEiOiJja2VhZWRhOG4wNmU5MnNxZXQ0bmhxZnU3In0.zWyuwunBSy51dulZG9gowQ';
 
+const GET_RECORDS = gql`
+  query getRecords {
+    records {
+      id
+      name
+      type {
+        name
+      }
+      coordinates {
+        latitude
+        longitude
+      }
+      fields {
+        name
+        value
+      }
+    }
+  }
+`;
 function Map() {
+  const { loading, error, data } = useQuery(GET_RECORDS);
+  // const [newData, setNewData] = useState(parkData.default.features);
+  // console.log("This is newdata! ",newData);
   const [viewport, setViewport] = useState({
     latitude: 15,
     longitude: 55,
@@ -37,7 +61,77 @@ function Map() {
         onViewportChange={viewport => {
           setViewport(viewport);
         }}
-      ></ReactMapGL>
+      >
+        {data &&
+          data.records.map(item => (
+            <Marker
+              key={item.id}
+              latitude={item.coordinates.latitude}
+              longitude={item.coordinates.longitude}
+            >
+              <button
+                className="marker-btn"
+                onClick={e => {
+                  e.preventDefault();
+                  setSelectedMark(item);
+                }}
+              >
+                {/* <img src="/skateboarding.svg" alt="Skatepark Icon"/> */}
+              </button>
+            </Marker>
+          ))}
+
+        {/* {console.log(parkData.default.features)} */}
+        {/* {newData.map((park) => (      
+        <Marker
+          key={park.properties.PARK_ID}
+          latitude={park.geometry.coordinates[1]}
+          longitude={park.geometry.coordinates[0]}
+          draggable={true}
+          // onDrag={(event) => {
+          //   // console.log(event.lngLat)
+          // }}
+          // onDragStart={(event) => {
+          //   // console.log("start!!!", event);
+          // }}
+          onDragEnd={(event) => {
+            setNewData(prev => {
+              return prev.map(m => 
+                m.properties.PARK_ID === park.properties.PARK_ID ? {
+                  ...m,
+                  geometry: {
+                    ...m.geometry, 
+                    coordinates: [event.lngLat[0], event.lngLat[1]]
+                  }
+                } : m
+              )
+            })
+          }}
+        >
+          <button className="marker-btn" onClick={(e) => {
+            e.preventDefault();
+            setSelectedMark(park);
+          }}>
+            <img src="/skateboarding.svg" alt="Skatepark Icon"/>
+          </button>
+        </Marker>
+      ))} */}
+
+        {selectedMark ? (
+          <Popup
+            latitude={selectedMark.geometry.coordinates[1]}
+            longitude={selectedMark.geometry.coordinates[0]}
+            onClose={() => {
+              setSelectedMark(null);
+            }}
+          >
+            <div>
+              <h2>{selectedMark.properties.NAME}</h2>
+              <p>{selectedMark.properties.DESCRIPTION}</p>
+            </div>
+          </Popup>
+        ) : null}
+      </ReactMapGL>
     </div>
   );
 }
