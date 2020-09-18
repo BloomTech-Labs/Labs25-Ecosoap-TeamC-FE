f6e365d14df5a89b96675cd26109fd8402ea0310
Mario Fernandez <diddleslip@gmail.com>
2020-09-18 12:04:14 -0400

    Removed previous commit changes due to bugs

--- src/components/pages/MainSideBar/MainSideBar.js
+++ src/components/pages/MainSideBar/MainSideBar.js
@@ -14,7 +14,7 @@ function Navbar() {
         </li>
         {NavBarData.map((item, index) => (
           <li key={index} className={item.cName}>
-            <Link to={item.path} target="_blank" rel="noopener noreferrer">
+            <Link to={item.path}>
               <span className="mainSideSpan">{item.title}</span>
               {item.icon}
             </Link>
