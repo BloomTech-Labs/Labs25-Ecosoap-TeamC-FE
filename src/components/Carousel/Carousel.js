7bfc6ebdd9f7250d778ef452c29b7d83529a9113
Mario Fernandez <diddleslip@gmail.com>
2020-09-18 04:58:44 -0400

    Initialized Carousel.js

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Carousel.css';
+
+function Carousel() {
+  return (
+    <div>
+      <h1>Hello Carousel!</h1>
+    </div>
+  );
+}
+
+export default Carousel;
